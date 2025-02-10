4749cbe4fc77423f07ed812cd54aa06f73e7cc51
Jeremy Fischer <jefische86@gmail.com>
2025-02-10 16:06:41 -0500

    challenges 1-4 completed

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
 require('dotenv').config()
+const upload = multer({ dest: 'uploads/' });
 
 var app = express();
 
@@ -12,6 +14,19 @@ app.get('/', function (req, res) {
 });
 
 
+// (1) You should provide your own project, no tthe example URL.
+// (2) You can submit a form that includes a file upload.
+// (3) The form file input field has the name attribute set to upfile.
+// (4) When you submit a file, you receive the file name, type, and size in bytes within the JSON response.
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+	console.log(req.file);
+	console.log(req.body);
+	res.json({
+		name: req.file.originalname,
+		type: req.file.mimetype,
+		size: req.file.size
+	});
+})
 
 
 const port = process.env.PORT || 3000;
